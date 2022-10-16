e40ce5ee16099b5207044d9741030163fd6038d7
FranzRivas-Ksquare <franz.rivas@itksquare.edu.mx>
2022-10-16 15:13:14 -0500

    finish activity

--- src/get_char_count.dart
+++ src/get_char_count.dart
@@ -1,16 +1,22 @@
-import 'dart:svg';
-
 Map<String, num> getCharCount(String word) {
   List<String> arrWord = word.split('');
   Map<String, num> match = {};
 
-  arrWord.forEach((element) {
-    if (!match.keys.contains(element)) {
-      match[element] = 1;
-    } else {
-      match[element]++;
+  for (int i = 0; i < arrWord.length; i++) {
+    if (!match.keys.contains(arrWord[i])) {
+      int count = 1;
+      for (int j = 0; j < arrWord.length; j++) {
+        if (i != j) {
+          if (arrWord[i] == arrWord[j]) {
+            count++;
+          }
+        }
+      }
+      match[arrWord[i]] = count;
     }
-  });
+  }
+  ;
+
   return match;
 }
 
